fbe5633e755873ccadeee570907889ac373e0bcc
Jorge <info@jorgebenitezlopez.com>
2024-06-10 22:53:41 +0200

    Animate

--- front/scripts/range.js
+++ front/scripts/range.js
@@ -17,12 +17,41 @@ function updateOpacity(value) {
           });
 }
 
+function updateModelViewerSrc(year) {
+          const modelViewerBig = document.querySelector('#modelViewerBig');
+          let newSrc;
+          if (year > 2045) {
+                    newSrc = './assets/glaciar/2050.glb';
+          } else if (year > 2040) {
+                    newSrc = './assets/glaciar/2045.glb';
+          } else if (year > 2035) {
+                    newSrc = './assets/glaciar/2040.glb';
+          } else if (year > 2030) {
+                    newSrc = './assets/glaciar/2035.glb';
+          } else if (year > 2025) {
+                    newSrc = './assets/glaciar/2030.glb';
+          } else {
+                    newSrc = './assets/glaciar/2025.glb';
+          }
+          modelViewerBig.setAttribute('src', newSrc);
+}
+
+
+function isSeguimosIgualSelected() {
+          return document.querySelector('#option-1').checked;
+}
+
 // Agregar event listeners a los inputs de rango
 document.querySelectorAll('.range-slider input[type="range"]').forEach(function (input) {
           input.addEventListener('input', function () {
+                    const year = parseInt(this.value, 10); // Ensure the value is an integer
                     var value = this.value;
                     console.log(value);
                     // Actualizar la opacidad de los glaciares
-                    updateOpacity(value);
+
+                    if (isSeguimosIgualSelected()) {
+                              updateOpacity(year);
+                              updateModelViewerSrc(year);
+                    }
           });
 });
